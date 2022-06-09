1b5b58116e67aea9ebc78fcebe43a5839e862508
josuhinrichs <josue2001marinho@gmail.com>
2022-06-09 18:46:05 -0300

    [i.front-H1] Adicionando tela de registro e implementando clique

--- ifila_app/app/src/main/java/com/example/ifila_app/MainActivity.kt
+++ ifila_app/app/src/main/java/com/example/ifila_app/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.ifila_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.ifila_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonRegistrar.setOnClickListener{ goToRegister(binding) }
+    }
+
+    fun goToRegister(binding: ActivityMainBinding){
+        val context = binding.root.context
+        val intent = Intent(context, RegisterScreen1::class.java)
+        context.startActivity(intent)
     }
 }
+
