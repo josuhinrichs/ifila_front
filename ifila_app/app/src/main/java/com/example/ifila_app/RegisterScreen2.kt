68c3c507881e8f8d45a22eebe455abeb15000cf4
SergioGarciaBF <sgbfgarcia@gmail.com>
2022-06-11 19:12:27 -0300

    [i.front-H1] Telas Inicial Registro 1 e Registro 2

---
+++ ifila_app/app/src/main/java/com/example/ifila_app/RegisterScreen2.kt
@@ -0,0 +1,11 @@
+package com.example.ifila_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterScreen2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_screen2)
+    }
+}
