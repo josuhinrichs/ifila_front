063fce8f3001a5b5e9359933a3fc456ba34bfcab
josuhinrichs <josue2001marinho@gmail.com>
2022-06-26 19:59:10 -0300

    [i.front-feature] Começando implementação da topbar no estabelecimento

---
+++ ifila_app/app/src/main/java/com/example/ifila_app/ManageQueue.kt
@@ -0,0 +1,12 @@
+package com.example.ifila_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManageQueue : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_queue)
+    }
+}
