dfbf1b457c4aa18079230c80e7b799a1953b2df3
SergioGarciaBF <sgbfgarcia@gmail.com>
2022-06-29 00:46:12 -0300

    [i.front-screen] Tela Código e Tela Estabelecimento

---
+++ ifila_app/app/src/main/java/com/example/ifila_app/EstabWithoutQueueScreen.kt
@@ -0,0 +1,11 @@
+package com.example.ifila_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EstabWithoutQueueScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_estab_without_queue_screen)
+    }
+}
