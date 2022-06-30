4fee890f46fc4a59c0c1a4d37b062c67f679f85c
SergioGarciaBF <sgbfgarcia@gmail.com>
2022-06-30 19:03:23 -0300

    [i.front-screens2] Fila usuario e fila estabelecimento

---
+++ ifila_app/app/src/main/java/com/example/ifila_app/ClientPosQueue2.kt
@@ -0,0 +1,11 @@
+package com.example.ifila_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClientPosQueue2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_client_pos_queue2)
+    }
+}
